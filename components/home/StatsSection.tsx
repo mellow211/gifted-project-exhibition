5761bcfd35f7092d2d8816c6cc6bec954b204ad3
mellow211 <mellow211@users.noreply.github.com>
2026-09-11 16:18:37 +0900

    Revert "feat: add robot advanced student Yang Yu-chan project and update total to 86 projects"
    This reverts commit 1b7fd54fcc7b77db1a8131caa3a099c80d1f9db5.

--- components/home/StatsSection.tsx
+++ components/home/StatsSection.tsx
@@ -7,8 +7,8 @@ import { FolderGit2, Users, Lightbulb } from "lucide-react";
 
 export const StatsSection: React.FC = () => {
   const [stats, setStats] = useState<ExhibitionStats>({
-    totalProjects: 86,
-    totalStudents: 86,
+    totalProjects: 85,
+    totalStudents: 85,
     totalFields: 5,
   });
 
